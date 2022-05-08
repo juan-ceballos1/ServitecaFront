6edec04083d579020d6710278a2d0e80654a18a6
Juan Esteban Ceballos Quintero - Ceiba Software <juan.ceballos@ceiba.com.co>
2022-05-08 00:45:42 -0500

    Se agrega un nueva test

--- src/app/feature/asistencia/components/crear-asistencia/crear-asistencia.component.spec.ts
+++ src/app/feature/asistencia/components/crear-asistencia/crear-asistencia.component.spec.ts
@@ -5,11 +5,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpService } from 'src/app/core/services/http.service';
 import { AsistenciaService } from '../../shared/service/asistencia.service';
+import { of } from 'rxjs';
 
 import { CrearAsistenciaComponent } from './crear-asistencia.component';
+import Swal from 'sweetalert2';
 
 describe('CrearAsistenciaComponent', () => {
   let component: CrearAsistenciaComponent;
+  let asistenciaService: AsistenciaService;
   let fixture: ComponentFixture<CrearAsistenciaComponent>;
 
   beforeEach(waitForAsync( () => {
@@ -30,10 +33,47 @@ describe('CrearAsistenciaComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CrearAsistenciaComponent);
     component = fixture.componentInstance;
+    asistenciaService= TestBed.inject(AsistenciaService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('formulario es invalido cuando esta vacio', () => {
+    expect(component.asistencia.valid).toBeFalsy();
+  });
+
+  it('Asistencia falla si no tiene idTipoAsistencia', () => {
+    
+    component.asistencia.controls.idTipoAsistencia.setValue(null);
+    component.asistencia.controls.idVehiculo.setValue('1');
+    component.asistencia.controls.fechaInicio.setValue('2020-05-05');
+    component.asistencia.controls.precio.setValue(34534);
+
+    component.crear();
+
+    expect(Swal.isVisible()).toBeTruthy();
+    expect(component.asistencia.valid).toBeFalsy();
+  });
+
+  it('Registrar asistencia es exitoso', () => {
+    
+    component.asistencia.controls.idTipoAsistencia.setValue('1');
+    component.asistencia.controls.idVehiculo.setValue('1');
+    component.asistencia.controls.fechaInicio.setValue('2020-05-05');
+    component.asistencia.controls.precio.setValue(34534);
+
+    spyOn(asistenciaService, 'crearAsistencia').and.returnValue(
+      of(1)
+    );
+
+    component.crear();
+
+    expect(Swal.isVisible()).toBeTruthy();
+    expect(Swal.getTitle().textContent).toEqual('Se ha añadido la asistencia de forma correcta');
+  });
+
+  
 });
--- src/app/feature/asistencia/components/crear-asistencia/crear-asistencia.component.ts
+++ src/app/feature/asistencia/components/crear-asistencia/crear-asistencia.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { TipoAsistencia } from 'src/app/feature/tipoasistencia/shared/model/tipoasistencia';
 import { TipoAsistenciaService } from 'src/app/feature/tipoasistencia/shared/service/tipo-asistencia.service';
+import Swal from 'sweetalert2';
 import { AsistenciaService } from '../../shared/service/asistencia.service';
 
 @Component({
@@ -20,13 +21,8 @@ export class CrearAsistenciaComponent implements OnInit {
 
   ngOnInit(): void {
     this.listaTipoAsistencias=this.tipoAsistenciaService.consultar();
-    this.asistencia = new FormGroup({
-      idTipoAsistencia: new FormControl(Number,[Validators.required]),
-      idVehiculo: new FormControl('',[Validators.required]),
-      fechaInicio: new FormControl('',[Validators.required]),
-      fechaFin: new FormControl('',[Validators.required]),
-      precio: new FormControl('',[Validators.required])
-    });
+    this.construirFormulario();
+    
   }
 
   crear(){
@@ -34,12 +30,27 @@ export class CrearAsistenciaComponent implements OnInit {
    this.asistencia.value.fechaInicio=this.datePipe.transform(this.asistencia.value.fechaInicio,'yyyy-MM-ddThh:mm:ss');
    console.log(this.asistencia.value)
    this.asistenciaService.crearAsistencia(this.asistencia.value).subscribe(
-    data=>{this.asistencia.reset()
-    console.log(data)
+    ()=>{
+      this.asistencia.reset()
+      Swal.fire({
+        icon:'success',
+        title:'Se ha añadido la asistencia de forma correcta'
+      })
     },
-    error=>console.log(error));
-   
-  
+    error=>Swal.fire({
+      icon:'error',
+      title:error.error.mensaje
+    }));
+  }
+
+  construirFormulario(){
+    this.asistencia = new FormGroup({
+      idTipoAsistencia: new FormControl(Number,[Validators.required]),
+      idVehiculo: new FormControl('',[Validators.required]),
+      fechaInicio: new FormControl('',[Validators.required]),
+      fechaFin: new FormControl(''),
+      precio: new FormControl('',[Validators.required])
+    });
   }
 
 }
